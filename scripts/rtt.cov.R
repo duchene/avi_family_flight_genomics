ecfe5555f8b08146194383bc73ced53d5ea3051b
David Alejandro Duchene Garzon <duchenedavid@gmail.com>
2025-07-17 10:16:18 +0200

    added data files as generated by scripts

--- scripts/rtt.cov.R
+++
@@ -1,6 +0,0 @@
-rtt.cov <- function(tre){
-        if(!is.rooted(tre)) tre <- midpoint(tre)
-        rtts <- pathnode(tre)[[1]]
-        rttcov <- sd(rtts, na.rm = T) / mean(rtts, na.rm = T)
-        return(rttcov)
-}
